4d53c369876af4f924749ce093d83ee29a35cbe4
Manuel Eiweck <manuel.eiweck@bkf.at>
2022-12-02 18:51:40 +0100

    add first ros python package

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # renewableAutomation
+
+
+# Slides
+
+## Extend ROS with your packages - Catkin
+
+
+``` bash 
+
+sudo apt install build-essential python-catkin-tools python-rosdep
+
+
+mkdir -p catkin_ws/src
+cd catkin_ws
+catkin init
+catkin config --extend /opt/ros/melodic
+``` 
+
+## Create a new package
+
+``` bash
+cd catkin_ws/src
+#catkin_create_pkg package_name package_dependencies
+catkin_create_pkg mmi_package rospy roscpp
+``` 
+
+## Build and run Python node
+
+``` bash 
+catkin build
+rosrun mmi_package mynode.py
+``` 
+
---
+++ catkin_ws/src/mmi_package/scripts/mynode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)#1hz
+
+    rospy.loginfo("Talker node started ...")
+
+
+    while not rospy.is_shutdown():
+        hello_str = "hello world {time}".format(time = rospy.get_time()) 
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try: 
+        talker()
+    except rospy.ROSInterruptException:
+        pass
